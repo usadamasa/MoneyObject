74c71a4ce3420b4b6af7b67d0d0ca21bc9b2f080
UCHIDA Masaru <parallelopipedon@gmail.com>
2011-06-18 14:36:57 +0900

    #francと#dollarの返り値をMoneyに変更した

--- src/money/Money.java
+++ src/money/Money.java
@@ -37,12 +37,12 @@ public class Money {
 		return currency;
 	}
 
-	public static Dollar dollar(int amount) {
-		return new Dollar(amount, "USD");
+	public static Money dollar(int amount) {
+		return new Money(amount, "USD");
 	}
 
-	public static Franc franc(int amount) {
-		return new Franc(amount, "CHF");
+	public static Money franc(int amount) {
+		return new Money(amount, "CHF");
 	}
 
 	@Override
