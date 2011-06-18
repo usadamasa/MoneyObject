c42f918c994333b949d5718db32e81ac1528a296
UCHIDA Masaru <parallelopipedon@gmail.com>
2011-06-18 14:40:48 +0900

    DollarとFrancをMoneyに統合した。

--- src/money/TestMoney.java
+++ src/money/TestMoney.java
@@ -15,19 +15,10 @@ public class TestMoney {
 		assertEquals(Money.dollar(15), five.times(3));
 	}
 
-/*	@Test
-	public void testFrancMultiplication() {
-		Franc five = new Franc(5);
-		assertEquals(new Franc(10), five.times(2));
-		assertEquals(new Franc(15), five.times(3));
-	}
-*/
 	@Test
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 
@@ -35,11 +26,5 @@ public class TestMoney {
 	public void testCurrency(){
 		assertEquals("USD",Money.dollar(1).currency());
 		assertEquals("CHF",Money.franc(1).currency());
-
-	}
-
-	@Test
-	public void testDifferentClassEquality(){
-		assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
 	}
 }
