e93ac9794f26ebdd4b3772af7c4d385dde9158e3
UCHIDA Masaru <parallelopipedon@gmail.com>
2011-06-18 17:01:47 +0900

    テストをパスした。

--- src/money/TestMoney.java
+++ src/money/TestMoney.java
@@ -73,4 +73,14 @@ public class TestMoney {
 	public void testIdentityRate(){
 		assertEquals(1,new Bank().rate("USD", "USD"));
 	}
+	
+	@Test
+	public void testMixedAddtion(){
+		Money fiveBucks = Money.dollar(5);
+		Money tenFrancs = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF","USD",2);
+		Money result = bank.reduce(fiveBucks.plus(tenFrancs),"USD");
+		assertEquals(Money.dollar(10),result);
+	}
 }
